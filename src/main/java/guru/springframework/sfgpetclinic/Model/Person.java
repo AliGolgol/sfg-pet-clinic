efd59c97e6f60c7a3f1a5ab42ab4b7d21ac808c4
aligolgol <golgol.postbox@gmail.com>
2019-09-13 20:31:33 +0430

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/Model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic.Model;
+
+public class Person {
+    private String firstName;
+    private String lastPerson;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastPerson() {
+        return lastPerson;
+    }
+
+    public void setLastPerson(String lastPerson) {
+        this.lastPerson = lastPerson;
+    }
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/Model/Pet.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.Model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+}
